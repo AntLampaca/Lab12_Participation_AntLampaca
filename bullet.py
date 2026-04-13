2341d704cc359e3a496f614105c37006677354d7
AntLampaca <inv0072@gmail.com>
2026-04-12 23:52:33 -0400

    Update bullet.py
    Added documentation

--- bullet.py
+++ bullet.py
@@ -1,7 +1,15 @@
 import pygame
 
 class Bullet(pygame.sprite.Sprite):
+    """
+    Represents a bullet fired by the player
+
+    - The bullet moves upward each frame and is automatically removed when it leaves the screen.
+    """
     def __init__(self, pos):
+        """
+        Creates a bullet at the given starting position.
+        """
         super().__init__()
         self.image = pygame.Surface((5, 10))
         self.image.fill("red")
@@ -9,6 +17,9 @@ class Bullet(pygame.sprite.Sprite):
         self.speed = 10
 
     def update(self):
+        """
+        Moves the bullet upward each frame
+        """
         self.rect.y -= self.speed
 
         if self.rect.bottom < 0:
