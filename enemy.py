10906762c9e0d1a22a30419cd771fb48eb95e22d
AntLampaca <inv0072@gmail.com>
2026-04-12 23:55:31 -0400

    Update enemy.py
    Added documentation

--- enemy.py
+++ enemy.py
@@ -1,7 +1,14 @@
 import pygame
 
 class Enemy(pygame.sprite.Sprite):
+    """
+    Represents a enemy in the game
+    (parts are outdated)
+    """
     def __init__(self, pos):
+        """
+        Initializes an enemy at a given position
+        """
         super().__init__()
         self.image = pygame.Surface((40,40))
         self.image.fill("green")
@@ -9,6 +16,11 @@ class Enemy(pygame.sprite.Sprite):
         self.speed = 2
     
     def update(self):
+        """
+        Move the enemy downward each frame.
+
+        removes enemy when it eaches the bottom
+        """
         self.rect.y += self.speed
 
         if self.rect.top > 800:
